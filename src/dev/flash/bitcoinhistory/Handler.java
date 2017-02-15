f2ad16c836047380e5a918ed982e4784b1b6a227
Flash <flash.jaq@gmail.com>
2017-02-15 10:18:55 +0000

    Created Initial Window

---
+++ src/dev/flash/bitcoinhistory/Handler.java
@@ -0,0 +1,13 @@
+package dev.flash.bitcoinhistory;
+
+/**
+ * Created by Flash on 15/02/2017.
+ */
+
+public class Handler {
+    private Instance instance;
+
+    public Handler (Instance instance){
+        this.instance = instance;
+    }
+}
---
+++ src/dev/flash/bitcoinhistory/Launcher.java
@@ -0,0 +1,13 @@
+package dev.flash.bitcoinhistory;
+
+/**
+ * Created by Flash on 15/02/2017.
+ */
+
+public class Launcher {
+    public static void main(String[] args){
+        Instance instance = new Instance("Bitcoin History", 1100, 600);
+        instance.start();
+    }
+}
+
